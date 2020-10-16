895a5157d34662bd176fd6bea6c1771fc67d9ec9
shubhamsugara <shubhamsugara22@gmail.com>
2020-10-16 13:04:29 +0530

    ll3

--- data_struc/linked_list/linked_list3.cpp
+++ data_struc/linked_list/linked_list3.cpp
@@ -1,11 +1,82 @@
-#include<stdio.h>
-#include <bits/stdc++.h> 
+// Iterative C++ program to reverse 
+// a linked list 
+#include <iostream> 
 using namespace std; 
 
 /* Link list node */
-class Node 
-{ 
-	public: 
+struct Node { 
 	int data; 
-	Node* next; 
-};
+	struct Node* next; 
+	Node(int data) 
+	{ 
+		this->data = data; 
+		next = NULL; 
+	} 
+}; 
+
+struct LinkedList { 
+	Node* head; 
+	LinkedList() 
+	{ 
+		head = NULL; 
+	} 
+
+	/* Function to reverse the linked list */
+	void reverse() 
+	{ 
+		// Initialize current, previous and 
+		// next pointers 
+		Node* current = head; 
+		Node *prev = NULL, *next = NULL; 
+
+		while (current != NULL) { 
+			// Store next 
+			next = current->next; 
+
+			// Reverse current node's pointer 
+			current->next = prev; 
+
+			// Move pointers one position ahead. 
+			prev = current; 
+			current = next; 
+		} 
+		head = prev; 
+	} 
+
+	/* Function to print linked list */
+	void print() 
+	{ 
+		struct Node* temp = head; 
+		while (temp != NULL) { 
+			cout << temp->data << " "; 
+			temp = temp->next; 
+		} 
+	} 
+
+	void push(int data) 
+	{ 
+		Node* temp = new Node(data); 
+		temp->next = head; 
+		head = temp; 
+	} 
+}; 
+
+/* Driver program to test above function*/
+int main() 
+{ 
+	/* Start with the empty list */
+	LinkedList ll; 
+	ll.push(20); 
+	ll.push(4); 
+	ll.push(15); 
+	ll.push(85); 
+
+	cout << "Given linked list\n"; 
+	ll.print(); 
+
+	ll.reverse(); 
+
+	cout << "\nReversed Linked list \n"; 
+	ll.print(); 
+	return 0; 
+} 
