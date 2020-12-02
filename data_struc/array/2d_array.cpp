35bb6b0c050878eb1127fa845d1d261d257cfc96
shubhamsugara22 <shubhamsugara22@gmail.com>
2020-12-02 20:25:31 +0530

    2darray

---
+++ data_struc/array/2d_array.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <conio.h>
+
+void lower(int matrix[3][3], int row, int col)
+{
+    int i, j;
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            if (i < j)
+            {
+                cout << "0"
+                     << " ";
+            }
+            else
+                cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+// Function to form upper triangular marix
+void upper(int matrix[3][3], int row, int col)
+{
+    int i, j;
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            if (i > j)
+            {
+                cout << "0"
+                     << " ";
+            }
+            else
+                cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int matrix[100][100], i, j;
+    int row, col;
+
+    cout << "enter the  number of rows and columns in 2-d arrrays" << endl;
+    cin >> row >> col;
+
+    for (i < 0; i < row; i++)
+    {
+        for (j < 0; j < col; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+    cout << "output matrix" << endl;
+    for (i < 0; i < row; i++)
+    {
+        for (j < 0; j < col; j++)
+        {
+            cout >> matrix[i][j] << / n;
+        }
+    }
+    cout << "Lower triangular matrix: \n";
+    lower(matrix, row, col);
+
+    cout << "Upper triangular matrix: \n";
+    upper(matrix, row, col);
+
+    return 0;
+}
