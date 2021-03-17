54bd53735befbf51b8ad7a0346b78c11b1196d71
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-03-17 20:06:50 +0530

    LL add node at start

--- data_struc/linked_list/linked_list.cpp
+++ data_struc/linked_list/linked_list.cpp
@@ -1,25 +1,34 @@
-// A simple CPP program to introduce 
-// a linked list 
-#include <bits/stdc++.h> 
-using namespace std; 
-class Node { 
-public: 
-	int data; 
-	Node* next; 
-}; 
+// A simple CPP program to introduce
+// a linked list
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+	int data;
+	Node *next;
+};
+// add new node at start
+void push_start(Node **head_ref, int new_data)
+{
+	Node *new_node = new Node();
+	new_node->data = new_data;
 
-// Program to create a simple linked 
-// list with 3 nodes 
-int main() 
-{ 
-	Node* head = NULL; 
-	Node* second = NULL; 
-	Node* third = NULL; 
+	new_node->next = (*head_ref);
+	(*head_ref) = new_node;
+}
+// Program to create a simple linked
+// list with 3 nodes
+int main()
+{
+	Node *head = NULL;
+	Node *second = NULL;
+	Node *third = NULL;
 
-	// allocate 3 nodes in the heap 
-	head = new Node(); 
-	second = new Node(); 
-	third = new Node(); 
+	// allocate 3 nodes in the heap
+	head = new Node();
+	second = new Node();
+	third = new Node();
 
 	/* Three blocks have been allocated dynamically. 
 	We have pointers to these three blocks as head, 
@@ -33,9 +42,9 @@ int main()
 # represents any random value. 
 Data is random because we haven’t assigned 
 anything yet */
-	head->data = 1; // assign data in first node 
-	head->next = second; // Link first node with 
-	// the second node 
+	head->data = 1;		 // assign data in first node
+	head->next = second; // Link first node with
+	// the second node
 
 	/* data has been assigned to the data part of first 
 	block (block pointed by the head). And next 
@@ -50,10 +59,10 @@ anything yet */
 	+---+---+	 +----+----+	 +-----+----+	 
 */
 
-	// assign data to second node 
-	second->data = 2; 
-	// Link second node with the third node 
-	second->next = third; 
+	// assign data to second node
+	second->data = 2;
+	// Link second node with the third node
+	second->next = third;
 	/* data has been assigned to the data part of the second 
 	block (block pointed by second). And next 
 	pointer of the second block points to the third 
@@ -65,8 +74,8 @@ anything yet */
 	+---+---+	 +---+---+	 +----+----+ 
 	| 1 | o----->| 2 | o-----> | # | # | 
 	+---+---+	 +---+---+	 +----+----+	 */
-	third->data = 3; // assign data to third node 
-	third->next = NULL; 
+	third->data = 3; // assign data to third node
+	third->next = NULL;
 
 	/* data has been assigned to the data part of the third 
 	block (block pointed by third). And next pointer 
@@ -86,7 +95,5 @@ anything yet */
 	Note that only the head is sufficient to represent 
 	the whole list. We can traverse the complete 
 	list by following the next pointers. */
-	return 0; 
-} 
-
-
+	return 0;
+}
