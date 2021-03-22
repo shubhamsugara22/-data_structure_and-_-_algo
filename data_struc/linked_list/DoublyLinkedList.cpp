10aa0e8f52726cb83f6f8efac4d3495a6ae60353
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-03-23 00:14:15 +0530

    added insert at start

--- data_struc/linked_list/DoublyLinkedList.cpp
+++ data_struc/linked_list/DoublyLinkedList.cpp
@@ -1,5 +1,5 @@
 //C++ program to demonstrate doubly linked list
-//Oeration 1)Insertion
+//Operation 1)Insertion
 #include <iostream.h>
 #include <conio.h>
 #include <stdio.h>
@@ -13,8 +13,28 @@ public:
     Node *next;
     Node *prev;
 };
+//enter new node at start of linedl ist
+void push(Node **head_ref, int new_data)
+{
+
+    //create new node
+    Node *new_node = new Node();
+    new_node->data = new_data;
+    //initialize next of new node to head and prev as null
+    new_node->next = *head_ref;
+    new_node->prev = NULL;
+    // change the prev of head node to new_node
+    if ((*head_ref) != NULL)
+    {
+        (*head_ref)->prev = new_node;
+    }
+    //move the head to new node
+    (*head_ref) = new_node
+}
 
 /* Driver program to test above functions*/
 int main()
 {
+    Node *head = NULL;
+    push(&head, 8)
 }
