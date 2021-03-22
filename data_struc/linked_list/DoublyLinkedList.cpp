d7831485619bb3e05b8c4be9f22dfd6c01b613a7
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-03-22 19:30:02 +0530

    Doubly linkedlist

---
+++ data_struc/linked_list/DoublyLinkedList.cpp
@@ -0,0 +1,20 @@
+//C++ program to demonstrate doubly linked list
+//Oeration 1)Insertion
+#include <iostream.h>
+#include <conio.h>
+#include <stdio.h>
+using namespace std;
+
+// A linked list node
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+};
+
+/* Driver program to test above functions*/
+int main()
+{
+}
