dbb43e2f7d913869c54b736da8d4917719827ae8
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-03-24 00:00:12 +0530

    changed compiler settings

--- data_struc/array/arr.cpp
+++ data_struc/array/arr.cpp
@@ -2,24 +2,22 @@
 
 using namespace std;
 
-int sum(int ar[] , int n)
+int sum(int ar[], int n)
 {
     int sm = 0;
-    for (int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        sm += ar[i];  
+        sm += ar[i];
     }
-return sm;    
+    return sm;
 }
 int main()
 {
-    int ar[] = { 12 ,5,7, 33} ;
-    int n = sizeof(ar)/sizeof(ar[0]);
-    cout << "Sum of given array " << sum(ar ,n) <<endl;
-    
-    
-    
-   // cout << "Hello World" << endl; 
-   
-   return 0;
+    int ar[] = {12, 5, 7, 33};
+    int n = sizeof(ar) / sizeof(ar[0]);
+    cout << "Sum of given array " << sum(ar, n) << endl;
+
+    // cout << "Hello World" << endl;
+
+    return 0;
 }
