15fd54ea11a48ee0fc91cd74d62af573e57a48b8
shubhamsugara22 <shubhamsugara22@gmail.com>
2021-03-25 17:00:24 +0530

    added all insert method

--- data_struc/linked_list/DoublyLinkedList.cpp
+++ data_struc/linked_list/DoublyLinkedList.cpp
@@ -1,5 +1,10 @@
 //C++ program to demonstrate doubly linked list
-//Operation 1)Insertion
+//Operation
+//1)Insertion
+//At the start
+//after a node
+//at the end
+//before a node
 #include <iostream>
 #include <conio.h>
 #include <stdio.h>
@@ -13,7 +18,7 @@ public:
     Node *next;
     Node *prev;
 };
-//enter new node at start of linked list
+//function to push  new node at start of linked list
 void push(Node **head_ref, int new_data)
 {
 
@@ -31,6 +36,7 @@ void push(Node **head_ref, int new_data)
     //move the head to new node
     (*head_ref) = new_node;
 }
+//function to add node after a node
 void insertafter(Node *prev_node, int new_data)
 {
     if (prev_node == NULL)
@@ -46,6 +52,62 @@ void insertafter(Node *prev_node, int new_data)
     if (new_node->next != NULL)
     {
         new_node->next->prev = new_node;
+    }
+}
+//function to add node at end
+void end(Node **head_ref, int new_data)
+{
+    //create new node
+    Node *new_node = new Node();
+    //store head value in last node
+    Node *last = *head_ref;
+    //enter data
+    new_node->data = new_data;
+    //point next of new node  ot null
+    new_node->next = NULL;
+    //check if linkedlist is empty
+    if (*head_ref == NULL)
+    {
+        new_node->prev = NULL;
+        *head_ref = new_node;
+        return;
+    }
+    //till last node is reached NULL
+    while (last->next != NULL)
+    {
+        last = last->next;
+    }
+    //last node next is null
+    last->next = new_node;
+    //new node prev point to last
+    new_node->prev = last;
+}
+//function to add new node before a node
+void insert_before(Node **head_ref, Node *next_node, int new_data)
+{
+    if (next_node == NULL)
+    {
+        cout << "given next node cannot be null";
+    }
+    //allocate new node
+    Node *new_node = new Node();
+    //insert new data
+    new_node->data = new_data;
+    //change prev of new node to  prev of next node
+    new_node->prev = next_node->prev;
+    //change of prev of next node to new node
+    next_node->prev = new_node;
+    //new node next points to next_node
+    new_node->next = next_node;
+    //change the next pointer of new node previous
+    if (new_node->prev != NULL)
+    {
+        new_node->prev->next = new_node;
+    }
+    //if prev node is NULL then new nodeis head
+    else
+    {
+        (*head_ref) = new_node;
     }
 }
 void printlist(Node *n)
@@ -65,8 +127,10 @@ int main()
     push(&head, 4);
     push(&head, 16);
     push(&head, 5);
+    end(&head, 67);
     // push after head +1
     insertafter(head->next, 7);
+    insert_before(&head, head->next->next, 9);
 
     cout
         << "the doubly linkedlist is "
